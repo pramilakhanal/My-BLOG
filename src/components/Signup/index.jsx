69ba7503e6302dc52c77915dddbd24ab54c9a173
pramila khanal <pramilakhanal21@gmail.com>
2018-08-31 13:26:33 -0600

    latest update

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -1,42 +1,52 @@
 
-import React from "react";
+import React from 'react';
+import Axios from 'axios';
+import { validateAll } from 'indicative'
 
-const Signup = () => {
+import config from '../../config'
 
-	return (
+import SignUpForm from './SignUpForm'
 
-		 <div className="mh-fullscreen bg-img center-vh p-20" style={{ backgroundImage: 'url(assets/img/bg-girl.jpg)' }}>
-      			<div className="card card-shadowed p-50 w-400 mb-0" style={{ maxWidth: '100%' }}>
-       				 <h5 className="text-uppercase text-center">Register</h5>
+class Signup extends React.Component {
+  constructor() {
+    super();
 
-   			 <br />
-   			 <br />
-   				 <form className="form-type-material">
-     					 <div className="form-group">
-       						 <input type="text" className="form-control" placeholder="Username" />
-      			</div>
-     					 <div className="form-group">
-        						<input type="text" className="form-control" placeholder="Email address" />
-      			</div>
-      					<div className="form-group">
-        						<input type="password" className="form-control" placeholder="Password" />
-      			</div>
-      					<div className="form-group">
-        						<input type="password" className="form-control" placeholder="Password (confirm)" />
-      			</div>
-      			<br />
-     					 <button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
-    			</form>
-   			 <hr className="w-30" />
-    					<p className="text-center text-muted fs-13 mt-20">Already have an account?
-      				<a href="login.html">Sign in</a>
-    				</p>
-  			</div>
-		</div>
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      errors: {}
+    };
+  }
 
-	);
-};
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
 
+  handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+        const user = await this.props.registerUser(this.state)
+        localStorage.setItem('user', JSON.stringify(user))
+        this.props.setAuthUser(user)
+        this.props.history.push('/');
+      } catch (errors) {
+        this.setState({ errors });
+      }
+  }
 
+  render() {
+    return (
+      <SignUpForm
+        handleInputChange={this.handleInputChange}
+        handleSubmit={this.handleSubmit}
+        errors={this.state.errors}
+      />
+    );
+  }
+}
 
-export default Signup; 
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, withRouter } from 'react-router-dom';
 
+import Login from './components/Login'
 import Navbar from './components/Navbar';
-
-import Login from './components/Login';
-
-import Footer from './components/Footer';
-
-import Welcome from './components/Welcome';
-
 import Signup from './components/Signup';
-
+import Footer from './components/Footer';
+import Welcome from './components/Welcome';
 import SingleArticle from './components/SingleArticle';
-
 import CreateArticle from './components/CreateArticle';
+
+import AuthService from './services/auth';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const Home = () => {
+class App extends React.Component {
 
-	return <h1>THIS IS THE HOME PAGE</h1>
-};
+  constructor() {
+    super();
 
-const About = () => {
+    this.state = {
+      authUser: null
+    };
+  }
 
-	return <h1>THIS IS THE ABOUT PAGE</h1>
-};
+  componentDidMount() {
+    const user = localStorage.getItem('user')
 
+    if (user) {
+      this.setState({
+        authUser: JSON.parse(user)
+      })
+    } 
+  }
+
+  setAuthUser = (authUser) => {
+    this.setState({
+      authUser
+    });
+  }
+
+  render() {
+    const { location } = this.props;
+    return (
+      <div>
+        {
+          location.pathname !== '/login' && location.pathname !== '/signup' &&
+          <Navbar authUser={this.state.authUser} />
+        }
+        <Route exact path="/" component={Welcome} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" render={
+            (props) => <Signup {...props}
+                registerUser={this.props.authService.registerUser}
+                setAuthUser={this.setAuthUser} />
+            } />
+        <Route path="/article/:slug" component={SingleArticle} />
+        <Route path="/articles/create" component={CreateArticle} />
+        {
+          location.pathname !== '/login' && location.pathname !== '/signup' &&
+          <Footer />
+        }
+      </div>
+    );
+  }
+}
+
+
+const Main = withRouter((props) => {
+  return (
+    <App authService={new AuthService()} {...props} />
+  );
+});
 
 ReactDOM.render(
-
-	<BrowserRouter>
-
-	<div>
-
-	<Navbar />
-
-	<Route exact path="/" component={Welcome} />
-
-	<Route path="/login" component={Login} />
-
-	<Route path="/signup" component={Signup} />
-
-	<Route path ="/article/:slug" component={SingleArticle} />
-
-	<Route path="/articles/create" component={CreateArticle} />
-
-
-	<Footer />
-
-	</div>
-
-	</BrowserRouter>
-
-
-, document.getElementById('root'));
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
